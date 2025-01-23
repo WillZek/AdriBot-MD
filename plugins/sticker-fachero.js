d92cac5482eb92aef87c0767215b05f8eee60ce0
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-23 14:30:38 -0600

    Actualizar sticker-fachero.js

--- plugins/sticker-fachero.js
+++ plugins/sticker-fachero.js
@@ -1,7 +1,14 @@
 let handler = async(m, { conn, usedPrefix, command }) => {
     const stickerUrl = 'https://files.catbox.moe/agx2sc.webp'; 
-m.react('😎')
+    m.react('😎');
+
     await conn.sendFile(m.chat, stickerUrl, 'sticker.webp', '', m, null);
+    
+    const groupMetadata = await conn.groupMetadata(m.chat);
+    const participants = groupMetadata.participants;
+    let mentions = participants.map(p => p.id).join(' ');
+    
+    await conn.sendMessage(m.chat, { text: `Mod Fachero 😎`, mentions: participants.map(p => p.id) });
 };
 
 handler.command = ['fachero'];
