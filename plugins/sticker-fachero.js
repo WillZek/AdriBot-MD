c94bab8cd6fb93427020516cda6ff84cedffc28e
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-23 14:20:02 -0600

    Actualizar sticker-fachero.js

--- plugins/sticker-fachero.js
+++ plugins/sticker-fachero.js
@@ -1,9 +1,15 @@
 let handler = async(m, { conn, usedPrefix, command, participants }) => {
+    try {
+        if (!participants || participants.length === 0) return m.reply('🎩 ¡No hay participantes para mencionar!');
 
-    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    const stickerUrl = 'https://files.catbox.moe/agx2sc.webp'; 
-m.react('😎')
-    await conn.sendFile(m.chat, stickerUrl, 'sticker.webp', '', { mentions: users }, m, null);
+        let users = participants.map(u => u.id).filter(v => v !== conn.user.jid);
+        const stickerUrl = 'https://files.catbox.moe/agx2sc.webp'; 
+        m.react('😎');
+        await conn.sendFile(m.chat, stickerUrl, 'sticker.webp', '', { mentions: users }, m, null);
+    } catch (error) {
+        console.error(error);
+        m.reply(`Ocurrió un error: ${error.message}`);
+    }
 };
 
 handler.command = ['fachero'];
