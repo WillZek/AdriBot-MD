9069568f23762f87c2fa5e1fec831cc346ae5370
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-31 14:15:03 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,19 +1,25 @@
-/* Mediafire By WillZek 
+/* Happy Mod Search By WillZek 
 - Free Codes Titan 
 - https://github.com/WillZek 
 */
+
+// 🎩 *HAPPYMOD - SEARCH*
 
 import fetch from 'node-fetch';
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return m.reply(`⬇️ Ingresa Un Texto Para Buscar En Happy Mod\n> Ejemplo: ${usedPrefix + command} Minecraft`);
+if (!text) return m.reply(`📩 Ingresa Un Texto Para Buscar En Happy Mod\n> Ejemplo: ${usedPrefix + command} Minecraft`);
 
 try {
 let api = `https://dark-core-api.vercel.app/api/search/happymod?key=api&text=${text}`;
 let response = await fetch(api);
 let json = await response.json();
 let arch = json.results;
+
+if (!arch || arch.length === 0) {
+    return m.reply(`🍭 No Encontramos Resultados Para : ${text}`);
+}
 
 m.react('🕑');
 let txt = `🍭 *Titulo:* ${arch.name}\n✏️ *Descripción:* ${arch.description}\n🌟 *Estrellas:* ${arch.stars}\n📎 *Link:* ${arch.link}`;
