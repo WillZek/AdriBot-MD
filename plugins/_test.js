fc2afeaacb337be3597abd024f0d4ad90811f2be
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-02-01 09:26:06 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -7,30 +7,19 @@
 import fetch from 'node-fetch';
 import { sticker } from '../lib/sticker.js';
 
-let handler = async(m, { conn, text, usedPrefix, command }) => {
+let handler = async(m, { conn, args, usedPrefix, command }) => {
 
-if (!text) return m.reply('🍭 Ingresa Un Texto Para Buscar Su Sticker');
+if (!args[0]) return m.reply('🍭 Ingresa Un Texto Para Buscar Su Sticker');
 m.react('🕑');
 
-let api = `https://api.diioffc.web.id/api/search/stickersearch?query=${text}`;
+let api = `https://delirius-apiofc.vercel.app/tools/htmlextract?url=${args[0]}`;
 let resp = await fetch(api);
 let json = await resp.json();
 let data = json.result;
 
-let stiker = false
-let img = data.sticker;
+let html = `*HTML EXTRAIDO DE LA WEB:*`
 
-
-let stickerUrl = img; 
-m.react('😎');
-stiker = await sticker(stickerUrl, false, global.packsticker, global.authsticker)
-
-/*
-await conn.sendFile(m.chat, stickerUrl, 'sticker.webp', '', m, null);
-*/
-
-// if (stiker) 
-conn.sendFile(m.chat, stickerUrl, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: '🎩𝐂𝐫𝐨𝐰𝐁𝐨𝐭 - 𝐒𝐭𝐢𝐜𝐤𝐞𝐫✨', body: `CrowBot - ST ${emojis}`, mediaType: 2, sourceUrl: grupo, thumbnail: icons}}}, { quoted: m })
+conn.sendMessage(m.chat, { caption: html }, { quoted: fkontak });
 };
 
 handler.command = ['test'];
