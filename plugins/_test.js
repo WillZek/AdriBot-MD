4d2fbbaaf5bbaf7ac00e59f31520f9993077f10f
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-31 19:29:57 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -23,7 +23,7 @@ let ttt = `*Autor:* ${res.author}\n*Título:* ${res.title}`;
 let aud = res.audio;
 let img = 'https://files.catbox.moe/51xcx4.jpg';
 
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', ttt, m, null, rcanal);
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', ttt, m, null);
 
 conn.sendMessage(m.chat, { audio: { url: aud }, mimetype: 'audio/mpeg' }, { quoted: m });
 m.react('✅');
