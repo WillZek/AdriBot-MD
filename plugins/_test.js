ce4b72bb349d767451e1b955cf79a9bc5ecf3710
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-31 20:20:33 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -18,7 +18,7 @@ let json = await response.json();
 let meta = json.meta[0];
 
 m.react('🕑');
-let txt = `✧ *Nombre De Usuario:* ${meta.nickname || 'No Disponible' }\n✧ *Titulo:* ${meta.title}\n✧ *Likes:* ${meta.like}\n✧ *Comentarios:* ${meta.coment}\n✧ *Compartidas:* ${meta.share}\n✧ *Link:* ${meta.url}`;
+let txt = `✧ *Titulo:* ${meta.title}\n✧ *Likes:* ${meta.like}\n✧ *Comentarios:* ${meta.coment}\n✧ *Compartidas:* ${meta.share}\n✧ *Link:* ${meta.url}`;
 
 let vid = meta.hd;
 
