cb9bb3865efd75138779ef8b87f9f6215ed51d1b
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-31 21:15:46 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -10,22 +10,9 @@ import fetch from 'node-fetch';
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
-/* if (!text) return m.reply(`🍭 Ingrese Un Texto Para Buscarlo En Tiktok\n> *Ejemplo:* ${usedPrefix + command} Crow Edits`);
-*/
-
-try {
-/* let api = `https://delirius-apiofc.vercel.app/search/tiktoksearch?query=${text}`;
-
-let response = await fetch(api);
-let json = await response.json();
-let meta = json.meta[0];
-*/
-
 m.react('🕑');
-/* let txt = `✧ *Titulo:* ${meta.title}\n✧ *Likes:* ${meta.like}\n✧ *Comentarios:* ${meta.coment}\n✧ *Compartidas:* ${meta.share}\n✧ *Link:* ${meta.url}`; 
-*/
 
-let txt = 'Pack🔥🔥🔥';
+let txt = 'Pack🔥🔥🔥\n> Pon De Nuevo .pack para mirar el siguiente ✨';
 
 let img = 'https://delirius-apiofc.vercel.app/nsfw/girls';
 
@@ -38,6 +25,6 @@ m.react('✖️');
  }
 };
 
-handler.command = ['test'];
+handler.command = ['pack'];
 
 export default handler;
