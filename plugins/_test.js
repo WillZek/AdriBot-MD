f4e4881901ab52fb959ec0412981282963662de7
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-02-01 08:29:07 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -18,7 +18,7 @@ let json = await resp.json();
 let data = json.result;
 
 let img = data.sticker[0];
-let sUrl = img; 
+let sUrl = 'https://s3.getstickerpack.com/storage/uploads/sticker-pack/croe-2020/sticker_1.png?7a91fa5d3be667dcab133716947394c5'; 
 
 m.react('✅');
 await conn.sendFile(m.chat, sUrl, 'sticker.webp', '', m, null);
